821f6881b20055be2155f91c7136a61c26e348a6
Presteniko Septi Rahadian <presteniko.jp@gmail.com>
2023-08-03 00:23:25 +0000

    add macro only

---
+++ macros/test_macro.sql
@@ -0,0 +1,16 @@
+{% macro ci_set_warehouse() %}
+
+    {{ (log("target_name: " ~ target_name, info=True)) }}
+    {{ set target_warehouse = 'QA_WH'}}
+
+    {%- if target.name == 'ci' -%}
+        {{ (log("Change Snowflake warehouse to: " ~ target_warehouse, info=True)) }}
+        {% call statement('ci_set_warehouse', fetch_result=True, auto_begin=False) -%}
+            USE WAREHOUSE {{ target_warehouse }}
+        {%- endcall -%}
+        {{ (log(load_result('ci_set_warehouse'), info=True))}}
+    {%- else -%}
+        {{ (log("Using default warehouse", info=True))}}
+    {%- endif -%}
+
+{% endmacro %}
